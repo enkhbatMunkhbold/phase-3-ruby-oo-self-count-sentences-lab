c75dc84ac58bb8cd72a5f5ed24a6edcc8a075fd9
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-11-05 15:31:42 -0700

    commit tree 28a0b7b4ca1b490bd6b25999ebe7cc26f3c8f21f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match?(/\.$/) 
   end
 
   def question?
-
+    self.match?(/\?$/)
   end
 
   def exclamation?
-
+    self.match?(/\!$/)
   end
 
   def count_sentences
-
+    def split_str(str)
+      str.split(/[\.\?\!]+/)
+    end
+      
+    split_str(self).length
   end
 end
